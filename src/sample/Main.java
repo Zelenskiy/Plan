2894db880b42047871a35377368f94c9bb00af19
Zelenskiy <a_s_zelenskiy@mail.ru>
2018-07-27 09:33:58 +0300

    1 commit
    Signed-off-by: Zelenskiy <a_s_zelenskiy@mail.ru>

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -16,6 +16,7 @@ public class Main extends Application {
         primaryStage.setMinWidth(800);
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
+
     }
 
 
